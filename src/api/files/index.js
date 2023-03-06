0c1b74ba84d9c6ff4a9dc45aa6338128d28262ec
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-06 12:58:26 +0100

    Adds Cloudinary

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,21 +1,32 @@
 import Express from "express"
 import multer from "multer"
 import { extname } from "path"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
 import { saveUsersAvatars } from "../../lib/fs-tools.js"
 
 const filesRouter = Express.Router()
 
-filesRouter.post("/:userId/single", multer().single("avatar"), async (req, res, next) => {
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary, // cloudinary is going to search for smth in .env vars called process.env.CLOUDINARY_URL
+    params: {
+      folder: "fs0522/users",
+    },
+  }),
+}).single("avatar")
+
+filesRouter.post("/:userId/single", cloudinaryUploader, async (req, res, next) => {
   // "avatar" here needs to match exactly to the name of the field appended to the FormData object in the FE (or in Postman req body)
   // If they do not match, multer will not find any file
   try {
     console.log("FILE:", req.file)
-    console.log("BODY:", req.body)
-    const originalFileExtension = extname(req.file.originalname)
-    const fileName = req.params.userId + originalFileExtension
-    await saveUsersAvatars(fileName, req.file.buffer)
+    // console.log("BODY:", req.body)
+    // const originalFileExtension = extname(req.file.originalname)
+    // const fileName = req.params.userId + originalFileExtension
+    // await saveUsersAvatars(fileName, req.file.buffer)
 
-    // Add an avatar field to the corresponding user in users.json file, containing `http://localhost:3001/img/users/${filename}`
+    // Add an avatar field to the corresponding user in users.json file, containing `https://res.cloudinary.com/riccardostrive/image/upload/v1678103158/fs0522/users/ufh2uwhbani8ttvho9zl.gif`
 
     res.send({ message: "file uploaded" })
   } catch (error) {
